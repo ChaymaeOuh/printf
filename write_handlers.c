d49679702268d2e76083db01d73fd53ff9b898a8
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2023-05-27 05:04:46 -0700

    Errors Fixed

--- write_handlers.c
+++ write_handlers.c
@@ -155,7 +155,7 @@ int my_write_num(int ind, char buffer[],
  *
  * Return: Number of written chars.
  */
-int write_unsgnd(int is_negative, int ind,
+int my_write_unsgnd(int is_negative, int ind,
 	char buffer[],
 	int flags, int width, int precision, int size)
 {
